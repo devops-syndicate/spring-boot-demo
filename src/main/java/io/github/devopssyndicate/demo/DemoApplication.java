9b58903bbabec6edbb85f27c98ffca827eb2bae1
sselzer <sascha.selzer@innoq.com>
2023-07-25 16:18:38 +0200

    Update

--- src/main/java/io/github/devopssyndicate/demo/DemoApplication.java
+++ src/main/java/io/github/devopssyndicate/demo/DemoApplication.java
@@ -1,13 +1,39 @@
 package io.github.devopssyndicate.demo;
 
+import io.pyroscope.http.Format;
+import io.pyroscope.javaagent.EventType;
+import io.pyroscope.javaagent.PyroscopeAgent;
+import io.pyroscope.javaagent.config.Config;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+
 @SpringBootApplication
 public class DemoApplication {
+    private final Optional<String> pyroscopeUrl;
+    private final String applicationName;
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public DemoApplication(@Value("${PYROSCOPE_URL:#{null}}") Optional<String> pyroscopeUrl, @Value("${spring.application.name}") String applicationName) {
+        this.pyroscopeUrl = pyroscopeUrl;
+        this.applicationName = applicationName;
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @PostConstruct
+    public void initPyroscope() {
+        this.pyroscopeUrl.ifPresent(url -> PyroscopeAgent.start(
+                new Config.Builder()
+                        .setApplicationName(this.applicationName)
+                        .setProfilingEvent(EventType.ITIMER)
+                        .setFormat(Format.JFR)
+                        .setServerAddress(url)
+                        .build()
+        ));
+    }
 }
